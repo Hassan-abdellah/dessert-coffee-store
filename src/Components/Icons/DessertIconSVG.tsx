94cd9fdef141b4cf9ec1a4da08114969dddbaed6
Hassan Abdellah <hassanabdellah25@gmail.com>
2026-02-28 00:14:58 +0200

    finalize navbar ui

---
+++ src/Components/Icons/DessertIconSVG.tsx
@@ -0,0 +1,144 @@
+const DessertIconSVG = ({
+  classNames = "stroke-rose-900",
+}: {
+  classNames?: string;
+}) => {
+  return (
+    <>
+      {/*?xml version="1.0" ?*/}
+      {/* Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools */}
+      <svg
+        width="20px"
+        height="20px"
+        viewBox="0 0 64 64"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <title />
+        <g id="donut">
+          <circle
+            cx={32}
+            cy={32}
+            r={29}
+            style={{
+              fill: "none",
+              strokeMiterlimit: 10,
+              strokeWidth: 2,
+            }}
+            className={classNames}
+          />
+          <circle
+            cx={32}
+            cy={32}
+            r={15}
+            style={{
+              fill: "none",
+              strokeMiterlimit: 10,
+              strokeWidth: 2,
+            }}
+            className={classNames}
+          />
+          <path
+            d="M17,37a2,2,0,0,0-2,2v1a2,2,0,0,1-4,0V26a2,2,0,0,0-4,0v5a2,2,0,0,1-4,0"
+            style={{
+              fill: "none",
+              strokeLinecap: "round",
+              strokeMiterlimit: 10,
+              strokeWidth: 2,
+            }}
+            className={classNames}
+          />
+          <path
+            d="M47,33a2,2,0,0,1,2,2v3a2,2,0,0,0,4,0V29a2,2,0,0,1,4,0v3a2,2,0,0,0,2,2h1"
+            style={{
+              fill: "none",
+              strokeLinecap: "round",
+              strokeMiterlimit: 10,
+              strokeWidth: 2,
+            }}
+            className={classNames}
+          />
+          <line
+            style={{
+              fill: "none",
+              strokeLinecap: "round",
+              strokeMiterlimit: 10,
+              strokeWidth: 2,
+            }}
+            x1={15}
+            x2={16}
+            y1={19}
+            y2={20}
+            className={classNames}
+          />
+          <line
+            style={{
+              fill: "none",
+              strokeLinecap: "round",
+              strokeMiterlimit: 10,
+              strokeWidth: 2,
+            }}
+            x1={18}
+            x2={19}
+            y1={12}
+            y2={11}
+            className={classNames}
+          />
+          <line
+            style={{
+              fill: "none",
+              strokeLinecap: "round",
+              strokeMiterlimit: 10,
+              strokeWidth: 2,
+            }}
+            x1={30}
+            x2={31}
+            y1={12}
+            y2={13}
+            className={classNames}
+          />
+          <line
+            style={{
+              fill: "none",
+              strokeLinecap: "round",
+              strokeMiterlimit: 10,
+              strokeWidth: 2,
+            }}
+            x1={38}
+            x2={39}
+            y1={9}
+            y2={9}
+            className={classNames}
+          />
+          <line
+            style={{
+              fill: "none",
+              strokeLinecap: "round",
+              strokeMiterlimit: 10,
+              strokeWidth: 2,
+            }}
+            x1={45}
+            x2={46}
+            y1={17}
+            y2={16}
+            className={classNames}
+          />
+          <line
+            style={{
+              fill: "none",
+              strokeLinecap: "round",
+              strokeMiterlimit: 10,
+              strokeWidth: 2,
+            }}
+            x1={52}
+            x2={52}
+            y1={21}
+            y2={22}
+            className={classNames}
+          />
+        </g>
+      </svg>
+    </>
+  );
+};
+
+export default DessertIconSVG;
